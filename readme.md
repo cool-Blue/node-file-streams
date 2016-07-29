a79d7282b06b2ea79a902e9e44d97dca254e6f2b
cool-Blue <cool.blue@y7mail.com>
2016-07-29 16:52:08 +1000

    added readme

---
+++ readme.md
@@ -0,0 +1,58 @@
+### stream.Readable emits `readable` twice
+##### If I set a listener on `readable`, the `readable` event fires again after `stream.read()`
+##### node version 5.12, windows 10
+### If I read immediately after creating the stream, I get null
+
+    stream = fs.createReadStream(inFile);
+    result = stream.read();
+![image](immediate.gif)
+### If I read after 200 msec, read succeeds and no further `readable` events are emitted
+
+    function _main() {
+        result = stream.read();
+    }
+    setTimeout(_main, delay);
+![image](timeout.gif)
+
+### If I read in a call-back on the `readable` event, read succeeds but a second `readable` event is emitted
+
+        stream.on('readable', () => {
+            result = stream.read()
+        });
+![image](onreabable.gif)
+
+### The unsimplified, main code is...: 
+
+    function main(repeats) {
+    
+        console.log(`\n${ESC}${BOLD};${RED}m${stamp()}starting main: ${repeats}${ESC}m`);
+    
+        // connect a readstream to the same file
+        stream = specs.Stream();
+    
+        // try to read it
+        if(options.immediate)
+            logit(`immediate`, 0);  // null
+    
+        if(options.timer)
+            logit(`after timeout`, options.timer);  // success, no more readable events
+    
+        if(!options["not-on-readable"])
+            stream.on('readable', (() => {
+                console.log(`${stamp()}setting listener on readable`)
+                var count = 0;
+                return () => {
+                    logit(`after readable:\t${++count}`, 0);
+                    if(repeats)
+                        stream.on('close', main.bind(this, --repeats))
+                }
+            })());  // success, but a second readable event is emitted
+        else
+            main.bind(this, --repeats);
+    
+        console.log(`${ESC}${BOLD};${RED}m${stamp()}ending main: ${repeats}${ESC}m\n`);
+    }
+
+    main(options.repeats);
+    
+### GIT Repo with complete working example
--- testFile.js
+++ testFile.js
@@ -1,18 +1,42 @@
 /**
- * Created by Admin on 7/28/2016.
+ * Created by cool.blue on 7/28/2016.
+ *
+ * read stream emits <readable> twice
+ *
+ * Usage: node testFile [-i][-r n][-n][-t x][-f]
+ *  -r is for n repeats (default 0)
+ *  -i is to force immediate read without waiting for <readable>.
+ *  -n to not read after <readable>
+ *  -t read after x msec
+ *  -f apply fix by adding and extra stream.read(1)
  */
-var fs     = require('fs');
-var logEvents = require('@cool-blue/logevents')();
+const fs = require('fs');
+const logEvents = require('@cool-blue/logevents')();
+
+const commandLineArgs = require('command-line-args');
+const optionDefinitions = [
+    {name: 'not-on-readable', alias: 'n', type: Boolean},
+    {name: 'immediate', alias: 'i', type: Boolean},
+    {name: 'repeats', alias: 'r', type: Number, defaultValue: 0},
+    {name: 'timer', alias: 't', type: Number, defaultValue: 0},
+    {name: 'fix', alias: 'f', type: Boolean}
+];
+const options = commandLineArgs(optionDefinitions);
+
 const now = require('moment');
 var specs = {
-        inFile: './test/fixtures/input.txt',
-        stream: function() {
+        inFile: './input.txt',
+        Stream: function() {
+
+            // create a new stream.Readable object connected to the input file
             var _stream = fs.createReadStream(this.inFile);
-            console.log(`${stamp()}read stream created`)
-            // log events from the stream
+            console.log(`${stamp()}read stream created`);
+
+            // log events from the Stream
             var streamEvents = ['pipe', 'unpipe', 'finish', 'cork', 'close', 'drain', 'error', 'end', 'readable'];
             logEvents.open((_stream.name = "stream", _stream), streamEvents);
-            // return the monitored stream
+
+            // return the monitored Stream
             return _stream
         },
     },
@@ -20,74 +44,56 @@ var specs = {
 
 var content = ["capture-this!", "日本語", "-k21.12-0k-ª–m-md1∆º¡∆ªº"];
 var stamp = () => `${now(Date.now()).format("HH:mm:s:SSSS")}\t`;
-var ESC = '\x1b[',
-    gEND = "m",
+var ESC    = '\x1b[',
+    gEND   = "m",
     allOFF = `${ESC}0m`,
-    BOLD = '1',
-    REV = '7',
-    WHITE = '37',
-    RED = '31',
-    GREEN = '32',
+    BOLD   = '1',
+    REV    = '7',
+    WHITE  = '37',
+    RED    = '31',
+    GREEN  = '32',
     YELLOW = '33',
-    BLUE = '34';
+    BLUE   = '34';
 
-function reset(inFile, content){
-
-    console.log(`${ESC}${BOLD};${BLUE}m${stamp()}reset${ESC}m`);
-    var _content;
-
-    //reset the test input
-    try {
-        fs.unlinkSync(inFile);
-    } catch(e) {
-        // just ignore it
-        console.warn("WARNING: inFile does not exist")
-    }
-
-    _content = content.reduce(function(res, line) {
-        return res + line + "\n";
-    }, "");
-    fs.writeFileSync(inFile, _content);
-
-    //return the file content for reference
-    return _content;
-}
-
-function logit (context, delay) {
+function logit(context, delay) {
     delay = delay || 0;
-    function _main(){
+    function _main() {
         result = stream.read();
-        stream.read(0);
         console.log(`\n${stamp()}${context}:\n${ESC}${WHITE}m${result}${ESC}m`)
     }
+
     if(delay) return setTimeout(_main, delay);
     return _main();
 }
 
 function main(repeats) {
-    console.log(`\n${ESC}${BOLD};${RED}m${stamp()}starting main${ESC}m`);
-    // prime the input file and return it's contents
-    body = reset(specs.inFile, content);
+
+    console.log(`\n${ESC}${BOLD};${RED}m${stamp()}starting main: ${repeats}${ESC}m`);
 
     // connect a readstream to the same file
-    stream = specs.stream();
+    stream = specs.Stream();
 
     // try to read it
-    /*
-     logit("immediate"); // null
+    if(options.immediate)
+        logit(`immediate`, 0);
 
-     logit('after timeout', 100);    // null if it happens after readable
+    if(options.timer)
+        logit(`after timeout`, options.timer);
 
-     stream.on('readable', () => {
-     logit('after readable + 0', 0); // success unless timeout happens first
-     });
-     */
+    if(!options["not-on-readable"])
+        stream.on('readable', (() => {
+            console.log(`${stamp()}setting listener on readable`);
+            var count = 0;
+            return () => {
+                logit(`after readable:\t${++count}`, 0);
+                if(repeats)
+                    stream.on('close', main.bind(this, --repeats))
+            }
+        })());
+    else
+        main.bind(this, --repeats);
 
-    stream.on('readable', () => {
-        logit(`after readable: ${repeats}`, 0);
-        if (repeats) stream.on('close', main.bind(this, --repeats))
-    });
-    console.log(`\n${ESC}${BOLD};${RED}m${stamp()}ending main${ESC}m`);
+    console.log(`${ESC}${BOLD};${RED}m${stamp()}ending main: ${repeats}${ESC}m\n`);
 }
 
-main(0);
+main(options.repeats);
